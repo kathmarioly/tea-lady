504da44ea56cc148863e517e25572ed93adb5fbe
kathmarioly <katerinemarioly@gmail.com>
2017-05-12 10:48:45 -0300

    Modificando archivos: html, js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function datos(nombre, apellido, correo, direccion, telefono)
+this.nombre = nombre;
+this.apellido = apellido;
+this.correo = correo;
+this.direccion = direccion;
+this.telefono = telefono;
+
+};
+
+function imprimir() {
+    var contenedor = document.getElementById("clientes");
+    var contenido = "";
+
+    //recorremos el arreglo listaStudent
+    listaStudent.forEach(function(datos) {
+        contenido += "<div>" + datos.nombre + "<br>" +
+            "Apellido:" + datos.apellido + "<br>" +
+            "Correo Electrónico:" + datos.correo+ "<br>" +
+            "Dirección" + datos.direccion + "<br>" + "Teléfono: " + datos.telefono + </div><br>";
+        console.log(contenido);
+    });
+
+    contenedor.innerHTML = contenido;
+}
